07c3745b494c0c393e86843ef87589b1ca1590f1
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-15 11:17:08 -0300

    Importo index.css, Componentes e implemento ruteo a secciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { Suspense, lazy } from "react"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-const Home = lazy(()=>import("./components/Home/Home"))
+import './assets/styles/index.css'
+import Header from "./components/Header"
 
+
+const Home = lazy(()=>import("./components/Home/Home"))
+const Hotel = lazy(()=>import("./components/Hotel/Hotel"))
+const About = lazy(()=>import("./components/About/About"))
+const Contact = lazy(()=>import("./components/Contact/Contact"))
+const Footer = lazy(()=>import("./components/Footer"))
 export default function App() {
   return (
     <Suspense fallback={<div className="loading">Cargando...</div>}>
       <BrowserRouter>
-
+        <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/inicio" element={<Home />} />
+            <Route path="/el-hotel" element={<Hotel />} />
+            <Route path="/sobre-nosotros" element={<About />} />
+            <Route path="/contacto" element={<Contact />} />
         </Routes>
-
+        <Footer />
       </BrowserRouter>
     </Suspense>
   )
