f8337141894c6203aaaa15adbc65369a4420ec11
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-21 13:18:56 -0300

    Agrego banner

---
+++ src/components/Home/HomeBanner.jsx
@@ -0,0 +1,15 @@
+import laChemineeLogo from '../../assets/logos/logo-la-cheminee-white.svg'
+
+export default function HomeBanner() {
+  return (
+    <section className="container-fluid col-12 row m-0 p-0 position-relative">
+
+      <div class="banner jumbotron-fluid d-flex justify-content-center align-items-center">
+        <div class="container d-flex flex-column justify-content-center align-items-center">
+          <h1 class="display-4">Bienvenidos a</h1>
+          <img src={laChemineeLogo} alt="Logo La Cheminee" className='col-8 img-fluid' />
+        </div>
+      </div>
+    </section>
+  )
+}
