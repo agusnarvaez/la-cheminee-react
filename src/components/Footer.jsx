15595a16878a7746740ff6e97af23bd54ef5300e
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-20 22:55:14 -0300

    Agrego estilos de footer en Desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
-import FooterTitle from "./Footer/FooterTitle";
-import FooterSocials from "./Footer/FooterSocials";
-import FooterAccordionItem from "./Footer/FooterAccordionItem";
+import FooterTitle from "./Footer/FooterTitle"
+import FooterSocials from "./Footer/FooterSocials"
+import FooterAccordionItem from "./Footer/FooterAccordionItem"
+import FooterNav from "./Footer/FooterNav"
+
 export default function Footer() {
+
 const primaryLinks = [
     {
         title:"El Hotel",
@@ -27,18 +30,33 @@ const secondaryLinks = [
         to:"/politicas-de-privacidad"
     }
 ]
-  return (
-    <footer className="container-fluid px-4 py-5 bg-green-la-cheminee">
+return (
+    <footer className="container-fluid px-4 py-5 bg-green-la-cheminee d-lg-flex ">
 
         <FooterTitle/>
 
-        <div className="accordion accordion-flush bg-green-la-cheminee" id="accordionFlush">
+        <div className="accordion accordion-flush bg-green-la-cheminee d-lg-none border-bottom" id="accordionFlush">
 
             <FooterAccordionItem title="La Cheminée" accordionId="flush-collapseOne" links={primaryLinks}/>
 
             <FooterAccordionItem title="Legales" accordionId="flush-collapseTwo" links={secondaryLinks}/>
 
         </div>
+
+
+        <ul className="d-none d-lg-flex col-5 container-fluid p-0 m-0">
+            <li className="container-fluid p-0 pe-3 m-0 col-6 ">
+                <h2 className="fs-6 fw-bold col-12 border-lg-0 p-0">La Cheminée</h2>
+                <FooterNav links={primaryLinks}/>
+            </li>
+            <li className="container-fluid p-0 pe-3 m-0 col-6 ">
+                <h2 className="fs-6 fw-bold col-12 border-lg-0 p-0">Legales</h2>
+                <FooterNav links={secondaryLinks}/>
+            </li>
+        </ul>
+
+
+
 
         <FooterSocials/>
     </footer>
--- src/components/Footer/FooterTitle.jsx
+++ src/components/Footer/FooterTitle.jsx
@@ -1,11 +1,11 @@
 import logoLaChemineeWhite from '../../assets/logos/logo-la-cheminee-white.svg'
 export default function FooterTitle() {
   return (
-    <section className="container-fluid d-flex flex-wrap justify-content-center mb-4">
+    <section className="container-fluid d-flex flex-wrap justify-content-center justify-content-lg-start mb-4 col-lg-5 m-lg-0 p-lg-0">
         <img className="img-fluid my-2" src={logoLaChemineeWhite} alt="Logo La Cheminée" />
-        <h3 className="text-white text-center col-12 fs-5 my-2">Hotel ubicado en San Martín de los Andes, Argentina</h3>
-        <h3 className="text-white text-center col-12 fs-5 my-2">Mariano Moreno Gral. Roca 8370</h3>
-        <h3 className="text-white text-center col-12 fs-5 my-2">Tel: +54 9 2972 54-1145</h3>
+        <h3 className="text-white text-center text-lg-start col-12 fs-5 my-2">Hotel ubicado en San Martín de los Andes, Argentina</h3>
+        <h3 className="text-white text-center text-lg-start col-12 fs-5 my-2">Mariano Moreno Gral. Roca 8370</h3>
+        <h3 className="text-white text-center text-lg-start col-12 fs-5 my-2">Tel: +54 9 2972 54-1145</h3>
     </section>
   )
 }
