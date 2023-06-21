76c31f8d17ead793e745753e63361c73211b852b
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-21 16:54:44 -0300

    Agrego introduccion de historia a la home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 import '../../assets/styles/home.css'
 
 import HomeBanner from "./HomeBanner"
+import HomeIntroduction from './HomeIntroduction'
+
 export default function Home() {
   return (
     <main className="container-fluid p-0">
 
       <HomeBanner/>
+
+      <HomeIntroduction/>
 
     </main>
   )
--- src/components/Home/HomeBanner.jsx
+++ src/components/Home/HomeBanner.jsx
@@ -7,7 +7,7 @@ export default function HomeBanner() {
       <div class="banner jumbotron-fluid d-flex justify-content-center align-items-center">
         <div class="container d-flex flex-column justify-content-center align-items-center">
           <h1 class="display-4">Bienvenidos a</h1>
-          <img src={laChemineeLogo} alt="Logo La Cheminee" className='col-8 col-lg-4 img-fluid' />
+          <img src={laChemineeLogo} alt="Logo La Cheminee" className='col-8 col-lg-6 col-xl-4 img-fluid' />
         </div>
       </div>
     </section>
