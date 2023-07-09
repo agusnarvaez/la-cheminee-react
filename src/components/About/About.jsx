6a08b8a0ae5b7522813a2bf8224903b4a1cba2e7
agusnarvaez <agus.narvaez@outlook.com>
2023-07-09 19:21:50 -0300

    Agrego otra sección de sobre nosotros

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,7 @@ import '../../assets/styles/index.css'
 import '../../assets/styles/about.css'
 import decoracion from '../../assets/images/decoracion-2.jpg'
 import ventana from '../../assets/images/ventana.jpg'
+import barra from '../../assets/images/barra.jpg'
 export default function About({metaData}) {
   return (
     <main className="container-fluid p-0 px-3 px-md-4 px-lg-5 d-flex flex-column align-items-center">
@@ -28,6 +29,27 @@ export default function About({metaData}) {
         <h2 className='texto-elegante text-center texto-verde-la-cheminee fs-2 m-0 lh-lg pt-3 px-2 fst-italic fw-light'>“Hospedarnos en La Cheminée fue una experiencia única. La comodidad, buena atención y calidez están presentes en todo momento. Volvería sin dudas”</h2>
         <p className='texto-verde-la-cheminee text-center fw-bold fs-1rem fs-md-13rem px-md-3 px-lg-5'>Mariana, 32 años. Huesped de La Cheminée</p>
       </div>
+    </div>
+
+    <div className="container-fluid p-0 py-0 py-lg-5 d-flex flex-column align-items-center my-5">
+      <h2 className='texto-elegante text-center texto-verde-la-cheminee fw-bold fs-1'>No se termina acá...</h2>
+      <p className='texto-verde-la-cheminee text-center fs-1rem fs-md-13rem px-md-3'>
+        En La Cheminée, nos enorgullece combinar la elegancia clásica con toques modernos para crear un ambiente acogedor y sofisticado, manteniendo la escencia que tanto caracterizaba al antiguo hotel. Nuestras habitaciones cuidadosamente decoradas ofrecen un refugio tranquilo donde podés relajarte y recargar energías después de un día explorando los encantos de la hermosa ciudad de San Martín de los Andes.
+      </p>
+
+      <p className='texto-verde-la-cheminee text-center fs-1rem fs-md-13rem px-md-3'>
+        Dentro del hotel, también ofrecemos una experiencia gastronómica de primera calidad. Nuestro talentoso equipo de chefs utiliza ingredientes frescos y locales para crear platos exquisitos que deleitarán tu paladar.
+      </p>
+
+      <p className='texto-verde-la-cheminee text-center fs-1rem fs-md-13rem px-md-3'>
+        Además de todo esto, podrás visitar nuestro Spa ubicado en las propias instalaciones del hotel para que puedas relajarte y disfrutar de todo lo que tenemos para ofrecerte: pileta, duchas escocesas, salón de masajes, sauna seco y húmedo, jacuzzi y sectores para que puedas deleitarte con bebidas y comida.
+      </p>
+
+      <p className='texto-verde-la-cheminee text-center fs-1rem fs-md-13rem px-md-3'>
+        Nuestro amable y atento equipo está dedicado a brindar un servicio personalizado, asegurándose de que cada huésped se sienta como en casa. Ya sea que necesites recomendaciones sobre los mejores lugares para visitar, reservas en restaurantes locales o asistencia con cualquier solicitud especial, estamos aquí para ayudarte en cada paso del camino.
+      </p>
+
+      <img alt='barra de la cheminée' className='img-fluid col-12 p-0 object-fit-cover mt-5 mb-md-0' src={barra} />
     </div>
 
     </main>
