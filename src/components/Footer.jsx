4996f0c6a8e04326b18cd8501578289e15aeb148
agusnarvaez <agus.narvaez@outlook.com>
2023-07-10 23:35:42 -0300

    Oculto términos y privacidad momentáneamente

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,7 +39,7 @@ return (
 
             <FooterAccordionItem title="La Cheminée" accordionId="flush-collapseOne" links={primaryLinks}/>
 
-            <FooterAccordionItem title="Legales" accordionId="flush-collapseTwo" links={secondaryLinks}/>
+            {/* <FooterAccordionItem title="Legales" accordionId="flush-collapseTwo" links={secondaryLinks}/> */}
 
         </div>
 
@@ -49,10 +49,10 @@ return (
                 <h2 className="fs-6 fw-bold col-12 border-lg-0 p-0">La Cheminée</h2>
                 <FooterNav links={primaryLinks}/>
             </li>
-            <li className="container-fluid p-0 pe-3 m-0 col-6 ">
+            {/* <li className="container-fluid p-0 pe-3 m-0 col-6 ">
                 <h2 className="fs-6 fw-bold col-12 border-lg-0 p-0">Legales</h2>
                 <FooterNav links={secondaryLinks}/>
-            </li>
+            </li> */}
         </ul>
 
         <FooterSocials/>
