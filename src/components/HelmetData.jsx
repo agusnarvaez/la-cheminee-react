80833255922aec8f982b23c931518daf84889e36
agusnarvaez <agus.narvaez@outlook.com>
2023-07-11 00:30:58 -0300

    Agrego imagen de banner a helmet

--- src/components/HelmetData.jsx
+++ src/components/HelmetData.jsx
@@ -1,7 +1,7 @@
 
 //* REACT-HELMET
 import { Helmet,HelmetProvider } from 'react-helmet-async'
-
+import laChemineeBanner from '../assets/backgrounds/banner-home.jpg'
 export default function HelmetData({metaData}) {
 
 return (
@@ -13,6 +13,10 @@ return (
         <meta property="og:title" content={`Quintas del Oeste | ${metaData.title}`}/>
 
         <meta name="keywords" content={metaData.keywords} />
+
+        <meta property="image" content={laChemineeBanner}/>
+        <meta property="og:image" content={laChemineeBanner}/>
+        <meta property="twitter:image" content={laChemineeBanner}/>
 
         <meta name="description" content={metaData.description} />
         <meta property="og:description" content={metaData.description}/>
