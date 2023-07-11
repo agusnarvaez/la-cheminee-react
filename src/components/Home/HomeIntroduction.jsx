a444363ce8f5eec81ebcdb76aef285fd8983bcbe
agusnarvaez <agus.narvaez@outlook.com>
2023-07-11 17:25:48 -0300

    Modificaciones galería principal home

--- src/components/Home/HomeIntroduction.jsx
+++ src/components/Home/HomeIntroduction.jsx
@@ -10,10 +10,10 @@ export default function HomeIntroduction() {
       <div className='container-fluid col-lg-6 my-4 mb-0 m-lg-0 px-4 row flex-column order-lg-1 justify-content-center align-items-center col-12'>
         <div className='row justify-content-start flex-lg-column p-0 align-items-end align-items-lg-start'>
           <img loading='lazy' src={laChemineeLogo} alt='La Cheminée' className='col-4 col-md-3 p-0 col-lg-5' />
-          <h2 className='col-8 fs-35rem m-0 p-0 ps-2 ps-lg-0  fw-bold texto-verde-la-cheminee texto-elegante lh-chico'>reabre sus puertas</h2>
+          <h2 className='col-8 col-lg-12 fs-15rem fs-md-2rem fs-xl-35rem m-0 p-0 ps-2 ps-lg-0  fw-bold texto-verde-la-cheminee texto-elegante lh-chico'>reabre sus puertas</h2>
         </div>
 
-        <p className='texto-verde-la-cheminee p-0 m-0 mt-3 fs-6 fs-lg-6'>
+        <p className='texto-verde-la-cheminee p-0 m-0 mt-3 mt-lg-4 pt-1 pe-xl-5 pe-xl-5 fs-1rem fw-light fs-lg-6'>
           El hotel se renovó para ofrecer un servicio acorde a las necesidades de sus huéspedes y convertirse en La Cheminée Hotel y Spa. Las instalaciones han sido mejoradas y pensadas para  poder brindar el mejor servicio a las diversas exigencias que la industria turística demanda a nivel internacional.
         </p>
 
