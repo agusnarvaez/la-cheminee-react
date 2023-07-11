8f9fe31e8f7e65220a288b95f72ac8902df9c35a
agusnarvaez <agus.narvaez@outlook.com>
2023-07-10 23:42:17 -0300

    Agrego loader a lazyLoad

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,13 @@ const metaData = {
 }
 
   return (
-    <Suspense fallback={<div className="loading">Cargando...</div>}>
+    <Suspense
+      fallback={
+        <div className="col-12 d-flex justify-content-center align-items-center">
+          <div className="spinner-border texto-verde-la-cheminee" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </div>}>
       <BrowserRouter>
         <Header />
         <Routes>
