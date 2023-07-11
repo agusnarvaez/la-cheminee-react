e5589e7f4bf3bef41d84f36876e9c5f0fae0d274
agusnarvaez <agus.narvaez@outlook.com>
2023-07-11 14:26:05 -0300

    Corrijo servicio tetera por pava eléctrica de las habitaciones

--- src/components/Home/HomeRooms.jsx
+++ src/components/Home/HomeRooms.jsx
@@ -15,19 +15,19 @@ const rooms = [
   {
     title: "Suite Standard",
     description: "Habitaciones ubicadas en el 1er piso. Las mismas cuentan con un espacio de 26m2.",
-    services: ["CAMA QUEEN",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','CAJA FUERTE','SECADOR DE PELO','TETERA'],
+    services: ["CAMA QUEEN",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','CAJA FUERTE','SECADOR DE PELO','PAVA ELÉCTRICA'],
     photos: [suiteStandard1,suiteStandard2,suiteStandard3]
   },
   {
     title: "Suite Standard Plus",
     description: "Habitaciones ubicadas en el 1er piso. Las mismas cuentan con un espacio de 38m2.",
-    services: ["CAMA KING",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','CAJA FUERTE','SECADOR DE PELO','TETERA'],
+    services: ["CAMA KING",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','CAJA FUERTE','SECADOR DE PELO','PAVA ELÉCTRICA'],
     photos: [suiteStandardPlus1,suiteStandardPlus2,suiteStandardPlus3]
   },
   {
     title: "Suite Superior",
     description: "Habitaciones ubicadas en el 2do y 3er piso. Las mismas cuentan con un espacio de 40m2.",
-    services: ["CAMA QUEEN",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','HOGAR','JACUZZI','CAJA FUERTE','SECADOR DE PELO','TETERA'],
+    services: ["CAMA QUEEN",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','HOGAR','JACUZZI','CAJA FUERTE','SECADOR DE PELO','PAVA ELÉCTRICA'],
     photos: [suiteSuperior1,suiteSuperior2,suiteSuperior3]
   }
 ]
