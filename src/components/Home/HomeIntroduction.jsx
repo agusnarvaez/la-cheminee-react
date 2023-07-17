eb6b8f27003c04e33948f63f3b98ca877586dccb
agusnarvaez <agus.narvaez@outlook.com>
2023-07-17 19:30:34 -0300

    Agrego fs-13rem a home introduction

--- src/components/Home/HomeIntroduction.jsx
+++ src/components/Home/HomeIntroduction.jsx
@@ -10,7 +10,7 @@ export default function HomeIntroduction() {
       <div className='container-fluid col-lg-6 my-4 mb-0 m-lg-0 px-4 row flex-column order-lg-1 justify-content-center align-items-center col-12'>
         <div className='row justify-content-start flex-lg-column p-0 align-items-end align-items-lg-start'>
           <img loading='lazy' src={laChemineeLogo} alt='La Cheminée' className='col-4 col-md-3 p-0 col-lg-5' />
-          <h2 className='col-8 col-lg-12 fs-15rem fs-md-2rem fs-xl-35rem m-0 p-0 ps-2 ps-lg-0  fw-bold texto-verde-la-cheminee texto-elegante lh-chico'>reabre sus puertas</h2>
+          <h2 className='col-8 col-lg-12 fs-13rem fs-md-2rem fs-xl-35rem m-0 p-0 ps-2 ps-lg-0  fw-bold texto-verde-la-cheminee texto-elegante lh-chico'>reabre sus puertas</h2>
         </div>
 
         <p className='texto-verde-la-cheminee p-0 m-0 mt-3 mt-lg-4 pt-1 pe-xl-5 pe-xl-5 fs-1rem fw-light'>
