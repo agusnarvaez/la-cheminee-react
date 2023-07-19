41e29564ea3547cdf17c9df1c662eccd5012ad99
agusnarvaez <agus.narvaez@outlook.com>
2023-07-18 23:07:05 -0300

    Agrego blurHash a habitaciones

--- src/components/Home/HomeRooms.jsx
+++ src/components/Home/HomeRooms.jsx
@@ -1,12 +1,22 @@
 import suiteStandard1 from '../../assets/images/suiteStandard-1.jpg'
 import suiteStandard2 from '../../assets/images/suiteStandard-2.jpg'
 import suiteStandard3 from '../../assets/images/suiteStandard-3.jpg'
+import suiteStandard4 from '../../assets/images/suiteStandard-4.jpg'
+import suiteStandard5 from '../../assets/images/suiteStandard-5.jpg'
 import suiteSuperior1 from '../../assets/images/suiteSuperior-1.jpg'
 import suiteSuperior2 from '../../assets/images/suiteSuperior-2.jpg'
 import suiteSuperior3 from '../../assets/images/suiteSuperior-3.jpg'
+import suiteSuperior4 from '../../assets/images/suiteSuperior-4.jpg'
+import suiteSuperior5 from '../../assets/images/suiteSuperior-5.jpg'
+import suiteSuperior6 from '../../assets/images/suiteSuperior-6.jpg'
 import suiteStandardPlus1 from '../../assets/images/suiteStandardPlus-1.jpg'
 import suiteStandardPlus2 from '../../assets/images/suiteStandardPlus-2.jpg'
 import suiteStandardPlus3 from '../../assets/images/suiteStandardPlus-3.jpg'
+import suiteStandardPlus4 from '../../assets/images/suiteStandardPlus-4.jpg'
+import suiteStandardPlus5 from '../../assets/images/suiteStandardPlus-5.jpg'
+import suiteStandardPlus6 from '../../assets/images/suiteStandardPlus-6.jpg'
+import suiteStandardPlus7 from '../../assets/images/suiteStandardPlus-7.jpg'
+import suiteStandardPlus8 from '../../assets/images/suiteStandardPlus-8.jpg'
 
 import HomeRoomsArticle from './HomeRooms/HomeRoomsArticle'
 
@@ -16,19 +26,94 @@ const rooms = [
     title: "Suite Standard",
     description: "Habitaciones ubicadas en el 1er piso. Las mismas cuentan con un espacio de 26m2.",
     services: ["CAMA QUEEN",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','CAJA FUERTE','SECADOR DE PELO','PAVA ELÉCTRICA'],
-    photos: [suiteStandard1,suiteStandard2,suiteStandard3]
+    photos: [
+      {
+        src:suiteStandard1,
+        blur: 'TUG*sW~pba-pbEIoD%M{ogt8t7jZ'
+      },
+      {
+        src:suiteStandard2,
+        blur: 'TsHUtjof-;_Nt7-p-:t7t7t7WWfj'
+      },
+      {
+        src:suiteStandard3,
+        blur:'TGDc5gIUxu~qIoogt7R*Rj?bRPWA'
+      },
+      {
+        src:suiteStandard4,
+        blur:'TEDlm9%N00~qjFDiIVWBxuR+Rjt7'
+      },{
+        src:suiteStandard5,
+      blur:'T4Ciw84:R600~C9Z?I9Z.8~qX8w]'
+    }]
   },
   {
     title: "Suite Standard Plus",
     description: "Habitaciones ubicadas en el 1er piso. Las mismas cuentan con un espacio de 38m2.",
     services: ["CAMA KING",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','CAJA FUERTE','SECADOR DE PELO','PAVA ELÉCTRICA'],
-    photos: [suiteStandardPlus1,suiteStandardPlus2,suiteStandardPlus3]
+    photos: [
+      {
+        src:suiteStandardPlus1,
+        blur: 'TIFrL.~pM_^%%MtSIURjRPoLaJM{'
+      },
+      {
+        src:suiteStandardPlus2,
+        blur: 'T6Bo~,N$0LR1Mxs,00xt~W5AxWxD'
+      },
+      {
+        src:suiteStandardPlus3,
+        blur:'TBDb]Bmi?H~4%K-p4m?wxAET-o$z'
+      },
+      {
+        src:suiteStandardPlus4,
+        blur:'TAA]pX?H0L014:={~W-;Io$eMxIo'
+      },
+      {
+        src:suiteStandardPlus5,
+        blur:'TPEBvyIp~Bs:ofn$axWVR+s9oIof'
+      },
+      {
+        src:suiteStandardPlus6,
+        blur:'THD8:?~VxvrqIURj9ZM{s:?b?Ht7'
+      },
+      {
+        src:suiteStandardPlus7,
+        blur:'T7D[|W00pHx^ozwa4T~q01F#acw['
+      },
+      {
+        src:suiteStandardPlus8,
+        blur:'TBAJZqIo0K~UbI9ajXj]j^RjR+xt'
+      }]
   },
   {
     title: "Suite Superior",
     description: "Habitaciones ubicadas en el 2do y 3er piso. Las mismas cuentan con un espacio de 40m2.",
     services: ["CAMA QUEEN",'TV 32"','WIFI','AIRE ACONDICIONADO','CALEFACCIÓN','HOGAR','JACUZZI','CAJA FUERTE','SECADOR DE PELO','PAVA ELÉCTRICA'],
-    photos: [suiteSuperior1,suiteSuperior2,suiteSuperior3]
+    photos: [
+      {
+        src:suiteSuperior1,
+        blur:'T8GR6t00JR0f-U~p9DOH?G14NFVY'
+      },
+      {
+        src:suiteSuperior2,
+        blur:'TPG*vj~X%habe,oIM|t8s,9Ge.sT'
+      },
+      {
+        src:suiteSuperior3,
+        blur:'TAEn^q000L^$9a589a~Bs-0fxuWT'
+      },
+      {
+        src:suiteSuperior4,
+        blur:'TTH1oN%MV?0M9us+9w%2xD~VWBM|'
+      },
+      {
+        src:suiteSuperior5,
+        blur:'TBF5gT0L#PPX$yrWT}NH$d_3ozD$'
+      },
+      {
+        src:suiteSuperior6,
+        blur:'TGE2tfJD$~~VR.xu00r;NH02s7e.'
+      }]
   }
 ]
   return (
--- src/components/Home/HomeRooms/HomeRoomsCarrousel/HomeRoomsCarrouselItem.jsx
+++ src/components/Home/HomeRooms/HomeRoomsCarrousel/HomeRoomsCarrouselItem.jsx
@@ -1,7 +1,16 @@
+import ImageComponent from "../../../ImageComponent"
 export default function CarrouselItem({photo,className}) {
   return (
     <div className={className}>
-      <img loading='lazy' src={photo} className="d-block w-100" alt={photo}/>
+      <ImageComponent
+        src={photo.src}
+        alt={photo.src}
+        className="d-block w-100"
+        blurClass=''
+        hash={photo.blur}
+        width='100%'
+        height='100%'
+      />
     </div>
   )
 }
